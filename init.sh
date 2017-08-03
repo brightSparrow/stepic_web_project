4b29bd9bd2d0bf044e8ee5c81fe7e89069bd5151
brightSparrow <sparrrow718@gmail.com>
2017-08-03 18:09:17 +0000

    Upgrade files

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 sudo rm /etc/nginx/sites-enabled/default
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/gunicorn.conf
 sudo /etc/init.d/gunicorn restart
-﻿sudo /etc/init.d/mysql start﻿
+sudo /etc/init.d/mysql start
